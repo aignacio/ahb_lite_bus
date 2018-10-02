a0921d262085ddfafbf8bf1261c3503e6f37f967
Ânderson Ignacio da Silva <anderson.silva@espressif.com>
2018-10-02 15:33:47 -0300

    Fixed ahb_node.sv that didn't route the correspondent hrdata from SLAVE 0, even if it was selected by address range.

--- rtl/ahb_node.sv
+++ rtl/ahb_node.sv
@@ -76,11 +76,10 @@ module ahb_node #(
   // HREADY SLAVE GENERATION
   // If slave selected, route master HREADY to the SLAVE HREADY input
   always_comb begin
+    hready_o[loop_1] = 1'b1;
     for ( loop_1 = 0; loop_1 < NB_SLAVES; loop_1++ ) begin
       if( hsel_o[loop_1] == 1'b1 ) begin
         hready_o[loop_1] = hready_i;
-      end else  begin
-        hready_o[loop_1] = 1'b1;
       end
     end
   end
@@ -88,11 +87,10 @@ module ahb_node #(
   // HWRITE SLAVE GENERATION
   // If slave selected, route master HWRITE to the SLAVE HWRITE input
   always_comb begin
+    hwrite_o[loop_2] = 1'b0;
     for ( loop_2 = 0; loop_2 < NB_SLAVES; loop_2++ ) begin
       if( hsel_o[loop_2] == 1'b1 ) begin
         hwrite_o[loop_2] = hwrite_i;
-      end else  begin
-        hwrite_o[loop_2] = 1'b0;
       end
     end
   end
@@ -100,11 +98,10 @@ module ahb_node #(
   // HMASTLOCK SLAVE GENERATION
   // If slave selected, route master HMASTLOCK to the SLAVE HMASTLOCK input
   always_comb begin
+    hmastlock_o[loop_3] = 1'b0;
     for ( loop_3 = 0; loop_3 < NB_SLAVES; loop_3++ ) begin
       if( hsel_o[loop_3] == 1'b1 ) begin
         hmastlock_o[loop_3] = hmastlock_i;
-      end else  begin
-        hmastlock_o[loop_3] = 1'b0;
       end
     end
   end
@@ -112,11 +109,10 @@ module ahb_node #(
   // HTRANS SLAVE GENERATION
   // If slave selected, route master HTRANS to the SLAVE HTRANS input
   always_comb begin
+    htrans_o[loop_4] = 2'b00;
     for ( loop_4 = 0; loop_4 < NB_SLAVES; loop_4++ ) begin
       if( hsel_o[loop_4] == 1'b1 ) begin
         htrans_o[loop_4] = htrans_i;
-      end else  begin
-        htrans_o[loop_4] = 2'b00;
       end
     end
   end
@@ -124,11 +120,10 @@ module ahb_node #(
   // HPROT SLAVE GENERATION
   // If slave selected, route master HPROT to the SLAVE HPROT input
   always_comb begin
+    hprot_o[loop_5] = 4'b0000;
     for ( loop_5 = 0; loop_5 < NB_SLAVES; loop_5++ ) begin
       if( hsel_o[loop_5] == 1'b1 ) begin
         hprot_o[loop_5] = hprot_i;
-      end else  begin
-        hprot_o[loop_5] = 4'b0000;
       end
     end
   end
@@ -136,11 +131,10 @@ module ahb_node #(
   // HBURST SLAVE GENERATION
   // If slave selected, route master HBURST to the SLAVE HBURST input
   always_comb begin
+    hburst_o[loop_6] = 3'b000;
     for ( loop_6 = 0; loop_6 < NB_SLAVES; loop_6++ ) begin
       if( hsel_o[loop_6] == 1'b1 ) begin
         hburst_o[loop_6] = hburst_i;
-      end else  begin
-        hburst_o[loop_6] = 3'b000;
       end
     end
   end
@@ -148,11 +142,10 @@ module ahb_node #(
   // HSIZE SLAVE GENERATION
   // If slave selected, route master HSIZE to the SLAVE HSIZE input
   always_comb begin
+    hsize_o[loop_7] = 3'b000;
     for ( loop_7 = 0; loop_7 < NB_SLAVES; loop_7++ ) begin
       if( hsel_o[loop_7] == 1'b1 ) begin
         hsize_o[loop_7] = hsize_i;
-      end else  begin
-        hsize_o[loop_7] = 3'b000;
       end
     end
   end
@@ -160,11 +153,10 @@ module ahb_node #(
   // HADDR SLAVE GENERATION
   // If slave selected, route master HADDR to the SLAVE HADDR input
   always_comb begin
+    hadrr_o[loop_8] = {AHB_ADDR_WIDTH{1'b0}};
     for ( loop_8 = 0; loop_8 < NB_SLAVES; loop_8++ ) begin
       if( hsel_o[loop_8] == 1'b1 ) begin
         hadrr_o[loop_8] = hadrr_i;
-      end else  begin
-        hadrr_o[loop_8] = {AHB_ADDR_WIDTH{1'b0}};
       end
     end
   end
@@ -172,23 +164,24 @@ module ahb_node #(
   // HWDATA SLAVE GENERATION
   // If slave selected, route master HWDATA to the SLAVE HWDATA input
   always_comb begin
+    hwdata_o[loop_9] = {AHB_DATA_WIDTH{1'b0}};
     for ( loop_9 = 0; loop_9 < NB_SLAVES; loop_9++ ) begin
       if( hsel_o[loop_9] == 1'b1 ) begin
         hwdata_o[loop_9] = hwdata_i;
-      end else  begin
-        hwdata_o[loop_9] = {AHB_DATA_WIDTH{1'b0}};
       end
     end
   end
 
   // HRDATA MASTER GENERATION
   // If slave selected, route slave HRDATA to the MASTER HRDATA input
+  // Pay attention to use this always descrip. with output attribution
+  // first THEN output changing according to for loop if/else approach
+  // makes simulator interpret wrong the sys. verilog construction
   always_comb begin
+    hrdata_o = {AHB_DATA_WIDTH{1'b0}};
     for ( loop_10 = 0; loop_10 < NB_SLAVES; loop_10++ ) begin
       if( hsel_o[loop_10] == 1'b1 ) begin
         hrdata_o = hrdata_i[loop_10];
-      end else  begin
-        hrdata_o = {AHB_DATA_WIDTH{1'b0}};
       end
     end
   end
@@ -196,11 +189,10 @@ module ahb_node #(
   // HRESP MASTER GENERATION
   // If slave selected, route slave HRESP to the MASTER HRESP input
   always_comb begin
+    hresp_o = 1'b0;
     for ( loop_11 = 0; loop_11 < NB_SLAVES; loop_11++ ) begin
       if( hsel_o[loop_11] == 1'b1 ) begin
         hresp_o = hresp_i[loop_11];
-      end else  begin
-        hresp_o = 1'b0;
       end
     end
   end
@@ -208,11 +200,10 @@ module ahb_node #(
   // HREADYOUT MASTER GENERATION
   // If slave selected, route slave HREADYOUT to the MASTER HREADYOUT input
   always_comb begin
+    hreadyout_o = 1'b1;
     for ( loop_12 = 0; loop_12 < NB_SLAVES; loop_12++ ) begin
       if( hsel_o[loop_12] == 1'b1 ) begin
         hreadyout_o = hreadyout_i[loop_12];
-      end else  begin
-        hreadyout_o = 1'b1;
       end
     end
   end
